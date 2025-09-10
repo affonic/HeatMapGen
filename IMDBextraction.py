0bcf2afeea410662b2c0f1e6579671b27dfaa910
Charlie <charliecass2004@gmail.com>
2025-09-10 20:33:23 +1000

    data is extracted, now it is also refined to just be the season, epnum, and rating

--- IMDBextraction.py
+++ IMDBextraction.py
@@ -2,8 +2,12 @@ import requests
 #import matplotlib
 import json
 
-def fetch_imdb_data(show_id):
-    url = f"https://api.imdbapi.dev/titles/{show_id}/episodes"
+Shows = {
+    "RickAndMorty" : "tt2861424"
+}
+
+def fetch_imdb_data(show_name):
+    url = f"https://api.imdbapi.dev/titles/{Shows.get(show_name)}/episodes"
 
     params = {"pageSize": 50}
     
@@ -21,9 +25,23 @@ def fetch_imdb_data(show_id):
             break
         params["pageToken"] = next_page
 
-    with open("imdb_data.json", "w", encoding="utf-8") as f:
+    with open(f"{show_name}.json", "w", encoding="utf-8") as f:
         json.dump(all_episodes, f, indent=2, ensure_ascii=False)
 
     print(all_episodes)
 
-fetch_imdb_data("tt2861424")
+def refine_imdb_data(show_name):
+    with open(f"{show_name}.json", "r") as file:
+        data = json.load(file)
+    for episode in data:
+        if "rating" not in episode:
+            continue
+        rating = episode.get("rating").get("aggregateRating")
+        epNum = episode.get("episodeNumber")
+        season = episode.get("season")
+        print(season, epNum, rating)
+        
+
+#fetch_imdb_data("RickAndMorty")
+
+refine_imdb_data("RickAndMorty")
